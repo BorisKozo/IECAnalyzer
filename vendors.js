809abe323c0255913ee86f683bf29473543d1195
Boris Kozorovitzky <boris.kozorovitzky@gmail.com>
2024-11-02 15:29:13 +0200

    Initial commit

---
+++ vendors.js
@@ -0,0 +1,260 @@
+window.vendors = {
+    cellcom: {
+        name: 'סלקום',
+        tracks: [
+            {
+                name: 'חוסכים קבוע שנה 1',
+                discount: 5,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים קבוע שנה 2',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים קבוע שנה 3',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים ביום',
+                discount: 15,
+                startTime: '07:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+            },
+            {
+                name: 'חוסכים למשפחה',
+                discount: 18,
+                startTime: '14:00',
+                endTime: '20:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים בלילה',
+                discount: 20,
+                startTime: '23:00',
+                endTime: '07:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+        ]
+    },
+    pazgaz: {
+        name: 'פזגז',
+        tracks: [
+            {
+                name: '27-7 קבוע',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+        ]
+    },
+    electra: {
+        name: 'אלקטרה',
+        tracks: [
+            {
+                name: 'היטק שנה 1',
+                discount: 8,
+                startTime: '23:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'היטק שנה 2',
+                discount: 9,
+                startTime: '23:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'היטק שנה 3',
+                discount: 10,
+                startTime: '23:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'power שנה 1',
+                discount: 5,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'power שנה 2',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'power שנה 3',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'לילה',
+                discount: 20,
+                startTime: '23:00',
+                endTime: '07:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+        ]
+    },
+    hot: {
+        name: 'הוט energy',
+        tracks: [
+            {
+                name: 'חוסכים 24-7 שנה 1',
+                discount: 5,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים 24-7 שנה 2',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים 24-7 שנה 3',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים ביום',
+                discount: 15,
+                startTime: '07:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+            },
+            {
+                name: 'חוסכים בלילה',
+                discount: 20,
+                startTime: '23:00',
+                endTime: '07:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+            },
+            {
+                name: 'חוסכים קבוע e-triple',
+                discount: 10,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                cap: 100,
+                comment: 'לקוחות HOT במסלול טריפל ו HOT MOBILE'
+            },
+            {
+                name: 'חוסכים קבוע HOT',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                comment: 'ללקוחות HOT במסלול טריפל'
+            },
+            {
+                name: 'חוסכים קבוע NEXT',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                comment: 'ללקוחות במסלול דאבל NEXT'
+            },
+        ]
+    },
+    partner: {
+        name: 'פרטנר חשמל',
+        tracks: [
+            {
+                name: 'הנחה קבועה שנה 1',
+                discount: 5,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'הנחה קבועה שנה 2',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'הנחה קבועה שנה 3',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'עובדים מהבית',
+                discount: 15,
+                startTime: '07:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+            },
+            {
+                name: 'חיות לילה',
+                discount: 20,
+                startTime: '23:00',
+                endTime: '07:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+        ]
+    },
+    bezeq: {
+        name: 'בזק energy',
+        tracks: [
+            {
+                name: 'חוסכים חכם שנה 1',
+                discount: 5,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים חכם שנה 2',
+                discount: 6,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים חכם שנה 3',
+                discount: 7,
+                startTime: '00:00',
+                endTime: '00:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+            {
+                name: 'חוסכים חכם ביום',
+                discount: 15,
+                startTime: '07:00',
+                endTime: '17:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+            },
+            {
+                name: 'חוסכים חכם בלילה',
+                discount: 20,
+                startTime: '23:00',
+                endTime: '07:00',
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            },
+        ]
+    }
+}
