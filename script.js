92584c4ef4201e31b814ecc2f6d0704006c5150a
Boris Kozorovitzky <boris.kozorovitzky@gmail.com>
2024-11-08 15:18:28 +0200

    Added by day chart

--- script.js
+++ script.js
@@ -105,30 +105,29 @@ function vendorsToRowData(vendors) {
 
 function calculateSavedValueForRow(row, parsedData) {
     let sum = 0;
-    const startTime = moment(row.startTime,'HH:mm');
-    const endTime = moment(row.endTime,'HH:mm');
-    console.log(startTime,endTime);
+    const startTime = moment(row.startTime, 'HH:mm');
+    const endTime = moment(row.endTime, 'HH:mm');
     parsedData.forEach((item) => {
         const itemDayOfWeek = item.fullDateMoment.format('ddd');
-        if (!row.days.includes(itemDayOfWeek)){ //Not in the correct day
+        if (!row.days.includes(itemDayOfWeek)) { //Not in the correct day
             return;
         }
-        if (row.startTime === row.endTime){ // if whole day just give the discount
+        if (row.startTime === row.endTime) { // if whole day just give the discount
             sum += item.value;
             return;
         }
-        const itemTime = moment(item.time,'HH:mm');
-        if (startTime.isBefore(endTime,'hour')){
-            if (itemTime.isBetween(startTime,endTime, 'hours','[)')){
+        const itemTime = moment(item.time, 'HH:mm');
+        if (startTime.isBefore(endTime, 'hour')) {
+            if (itemTime.isBetween(startTime, endTime, 'hours', '[)')) {
                 sum += item.value;
             }
         } else {
-            if (itemTime.isAfter(startTime,'hours') || itemTime.isBefore(endTime,'hours')){
+            if (itemTime.isAfter(startTime, 'hours') || itemTime.isBefore(endTime, 'hours')) {
                 sum += item.value;
             }
         }
     });
-   return sum*(row.discount/100)*COST_OF_ELECTRICITY_UNIT;
+    return sum * (row.discount / 100) * COST_OF_ELECTRICITY_UNIT;
 }
 
 function calculateSavedValue(rowData, parsedData) {
@@ -137,16 +136,16 @@ function calculateSavedValue(rowData, parsedData) {
     });
 }
 
-function daysFormatter(params){
-    if (params.value.length === 7){
+function daysFormatter(params) {
+    if (params.value.length === 7) {
         return 'בכל ימות השבוע';
     }
-    if (params.value.every(value => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'].includes(value))){
+    if (params.value.every(value => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'].includes(value))) {
         return 'א-ה';
     }
 }
 
-function showGrid(parsedData, vendors) {
+function showSuppliersGrid(parsedData, vendors) {
     const rowData = vendorsToRowData(vendors);
     calculateSavedValue(rowData, parsedData);
 
@@ -160,26 +159,107 @@ function showGrid(parsedData, vendors) {
             {field: "title", headerName: 'מסלול'},
             {field: "startTime", headerName: 'שעת התחלה', width: 120},
             {field: "endTime", headerName: 'שעת סיום', width: 120},
-            {field: "days", headerName: 'ימים', valueFormatter:daysFormatter},
+            {field: "days", headerName: 'ימים', valueFormatter: daysFormatter},
             {field: "discount", headerName: 'הנחה', valueFormatter: p => p.value + '%', width: 100},
             {field: "comment", headerName: 'הגבלות', width: 520},
             {field: "cap", headerName: 'חיסכון מקסימלי בחודש', width: 200},
-            {field: 'savedValue', headerName: 'חיסכון בתקופת זמן', width: 200,  valueFormatter: p => Math.round(p.value)},
+            {
+                field: 'savedValue',
+                headerName: 'חיסכון בתקופת זמן',
+                width: 200,
+                valueFormatter: p => Math.round(p.value)
+            },
         ]
     };
     const myGridElement = document.getElementById('suppliers-grid');
     agGrid.createGrid(myGridElement, gridOptions);
 }
 
+function showByTime(parsedData) {
+
+    const dataMap = new Map();
+    parsedData.forEach((item) => {
+        if (dataMap.has(item.time)) {
+            dataMap.set(item.time, dataMap.get(item.time) + item.value);
+        } else {
+            dataMap.set(item.time, item.value);
+        }
+    });
+
+    const data = [];
+    dataMap.forEach((value, key) => {
+        data.push([key, value]);
+    });
+
+console.log(data);
+
+    Highcharts.chart('by-time-container', {
+        chart: {
+            zooming: {
+                type: 'x'
+            },
+            panning: true,
+            panKey: 'shift'
+        },
+        title: {
+            text: 'Electricity usage per time',
+            align: 'left'
+        },
+        subtitle: {
+            subtitle: {
+                text: 'Click and drag to zoom in. Hold down shift key to pan.',
+                align: 'left'
+            },
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'KW'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'line',
+            name: 'KW/h',
+            data: data
+        }]
+    });
+}
+
+function showAll(parsedData, vendors) {
+    drawAllPoints(parsedData);
+    showSuppliersGrid(parsedData, vendors);
+    showByTime(parsedData);
+}
+
 window.onload = () => {
     const parsedData = parseInput(window.fake_data);
-    drawAllPoints(parsedData);
-    showGrid(parsedData, window.vendors);
+    showAll(parsedData, window.vendors);
 }
 
 function loadData() {
     const data = document.getElementById('floating-textarea').value ?? '';
     const parsedData = parseInput(data);
-    drawAllPoints(parsedData);
-    showGrid(parsedData, window.vendors);
+    showAll(parsedData, window.vendors);
 }
